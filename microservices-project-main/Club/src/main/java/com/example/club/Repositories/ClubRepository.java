f7f3e81a14ca645d183d66686ee992b627201f1c
Arij-Maaroufi <106880747+Arij-Maaroufi@users.noreply.github.com>
2024-07-22 16:28:01 +0100

    Delete microservices-project-main directory

--- microservices-project-main/Club/src/main/java/com/example/club/Repositories/ClubRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.club.Repositories;
-
-import com.example.club.Models.Club;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClubRepository extends JpaRepository<Club, Long> {
-}
